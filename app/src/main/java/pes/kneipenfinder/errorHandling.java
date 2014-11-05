1d663c31289a5d6711b4ae168c12c1e8ec15ead3
Peter <peter.sieverding@googlemail.com>
2014-11-05 08:56:23 +0100

    Error Handling

---
+++ app/src/main/java/pes/kneipenfinder/errorHandling.java
@@ -0,0 +1,13 @@
+package pes.kneipenfinder;
+
+/**
+ * Created by Peter on 05.11.2014.
+ */
+public class errorHandling {
+
+    // Log Eintrag in DB schreiben
+    public void writeLog(String action, String text){
+
+    }
+
+}
