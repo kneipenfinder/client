d28a1b30ab1a0d537639a99d56a70a6f4000c374
Peter <peter.sieverding@googlemail.com>
2014-11-29 09:14:54 +0100

    add location checks the input

--- app/src/main/java/pes/kneipenfinder/add.java
+++ app/src/main/java/pes/kneipenfinder/add.java
@@ -1,9 +1,12 @@
 package pes.kneipenfinder;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -12,14 +15,138 @@ import android.widget.EditText;
 
 public class add extends Activity {
 
-    EditText name;
-    EditText street;
-    EditText postcode;
+    private EditText name;
+    private EditText street;
+    private EditText postcode;
+    final Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
+        name = (EditText) findViewById(R.id.addName);
+        name.setFocusable(false);
+        name.setClickable(true);
+        street = (EditText) findViewById(R.id.addStreet);
+        street.setFocusable(false);
+        street.setClickable(true);
+        postcode = (EditText) findViewById(R.id.addPostcode);
+        postcode.setFocusable(false);
+        postcode.setClickable(true);
+
+        // On Click Listener für den Namen der Location
+        name.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // hole das Alert Layout
+                LayoutInflater layoutInflater = LayoutInflater.from(context);
+
+                View promptView = layoutInflater.inflate(R.layout.alert_name, null);
+
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
+
+                alertDialogBuilder.setView(promptView);
+
+                final EditText input = (EditText) promptView.findViewById(R.id.userInput);
+
+                // Erstelle das Fenster
+                alertDialogBuilder
+                        .setCancelable(false)
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                // Hole den User Input und schreibe ihn ins Textfeld
+                                    name.setText(input.getText());
+                            }
+                        })
+                        .setNegativeButton("Cancel",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog,	int id) {
+                                        dialog.cancel();
+                                    }
+                                });
+
+                // Erstelle einen Alert Dialog
+                AlertDialog alertD = alertDialogBuilder.create();
+
+                alertD.show();
+            }
+        });
+
+        // On Click Listener für die Straße der Location
+        street.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // hole das Alert Layout
+                LayoutInflater layoutInflater = LayoutInflater.from(context);
+
+                View promptView = layoutInflater.inflate(R.layout.alert_street, null);
+
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
+
+                alertDialogBuilder.setView(promptView);
+
+                final EditText input = (EditText) promptView.findViewById(R.id.userInput);
+
+                // Erstelle das Fenster
+                alertDialogBuilder
+                        .setCancelable(false)
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                // Hole den User Input und schreibe ihn ins Textfeld
+                                street.setText(input.getText());
+                            }
+                        })
+                        .setNegativeButton("Cancel",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog,	int id) {
+                                        dialog.cancel();
+                                    }
+                                });
+
+                // Erstelle einen Alert Dialog
+                AlertDialog alertD = alertDialogBuilder.create();
+
+                alertD.show();
+            }
+        });
+
+        // On Click Listener für die Postleitzahl der Location
+        postcode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // hole das Alert Layout
+                LayoutInflater layoutInflater = LayoutInflater.from(context);
+
+                View promptView = layoutInflater.inflate(R.layout.alert_street, null);
+
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
+
+                alertDialogBuilder.setView(promptView);
+
+                final EditText input = (EditText) promptView.findViewById(R.id.userInput);
+
+                // Erstelle das Fenster
+                alertDialogBuilder
+                        .setCancelable(false)
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                // Hole den User Input und schreibe ihn ins Textfeld
+                                postcode.setText(input.getText());
+                            }
+                        })
+                        .setNegativeButton("Cancel",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog,	int id) {
+                                        dialog.cancel();
+                                    }
+                                });
+
+                // Erstelle einen Alert Dialog
+                AlertDialog alertD = alertDialogBuilder.create();
+
+                alertD.show();
+            }
+        });
     }
 
     public void button_submitLocation(View v){
@@ -42,6 +169,7 @@ public class add extends Activity {
     private boolean checkName() {
         String sName = name.getText().toString();
         if(sName.isEmpty()){
+            allertForm("Name wurde nicht eingegeben.");
             return false;
         }
         return true;
@@ -50,7 +178,7 @@ public class add extends Activity {
     private boolean checkStreet() {
         String sStreet = street.getText().toString();
         if(sStreet.isEmpty()){
-            return false;
+            allertForm("Straße wurde nicht eingegeben.");
         }
         return true;
     }
@@ -58,9 +186,27 @@ public class add extends Activity {
     private boolean checkPostcode() {
         String sPostcode = postcode.getText().toString();
         if(sPostcode.isEmpty()){
+            allertForm("Postleitzahl wurde nicht angegeben.");
             return false;
         }
         return true;
+    }
+
+    // Form, die aufgerufen wird, falls eine falsche Eingabe getätigt wurde
+    public void allertForm(String message){
+        AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
+        builder1.setTitle("Eingaben nicht korrekt");
+        builder1.setMessage(message);
+        builder1.setCancelable(true);
+        builder1.setNeutralButton(android.R.string.ok,
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+
+        AlertDialog alert11 = builder1.create();
+        alert11.show();
     }
 
     @Override
